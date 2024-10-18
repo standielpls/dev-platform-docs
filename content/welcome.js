510b7f696d3e7249ae111819383347300ec4c0ba
szchenghuang <6309412+szchenghuang@users.noreply.github.com>
2024-10-18 23:24:10 +0800

    Zapier session fetched.

--- content/welcome.js
+++ content/welcome.js
@@ -2,3 +2,11 @@ console.info(
   '%cDig into the Zapier Developer Platform!',
   'font-weight: bold; font-size: 1rem; text-shadow: #474747 2px 2px 2px, 2px 1px 1px rgba(255,79,0,0); color: #FFFFFF; background: #FF4F00; padding: 5px; border: 1px solid #ffffff; border-radius: 2px;'
 );
+
+fetch("https://zapier.com/api/v4/session")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (session) {
+    console.info({ session });
+  });
