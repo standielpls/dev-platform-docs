db74f6ecb0dcf32319c62a358012b096c20d63a8
szchenghuang <6309412+szchenghuang@users.noreply.github.com>
2024-10-19 00:01:00 +0800

    Session fetched with credentials sent along.

--- content/welcome.js
+++ content/welcome.js
@@ -3,7 +3,7 @@ console.info(
   'font-weight: bold; font-size: 1rem; text-shadow: #474747 2px 2px 2px, 2px 1px 1px rgba(255,79,0,0); color: #FFFFFF; background: #FF4F00; padding: 5px; border: 1px solid #ffffff; border-radius: 2px;'
 );
 
-fetch("https://zapier.com/api/v4/session")
+fetch('https://zapier.com/api/v4/session', {credentials: 'include'})
   .then(function (response) {
     return response.json();
   })
